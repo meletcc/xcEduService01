efc23a7fd25c6aba58c160927e30c55195714267
meletcc <840290380@qq.com>
2019-10-22 16:37:36 +0800

    学成网项目

--- test-fastdfs/src/test/java/com/xuecheng/test/fastdfs/TestFastDFS.java
+++ test-fastdfs/src/test/java/com/xuecheng/test/fastdfs/TestFastDFS.java
@@ -12,62 +12,57 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 /**
- * @author Administrator
- * @version 1.0
+ * 测试fastDfs的使用
  **/
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class TestFastDFS {
 
-    //上传文件
+    // 上传文件
     @Test
-    public void testUpload(){
-
+    public void testUpload() {
         try {
-            //加载fastdfs-client.properties配置文件
+            // 加载fastdfs-client.properties配置文件
             ClientGlobal.initByProperties("config/fastdfs-client.properties");
-            //定义TrackerClient，用于请求TrackerServer
+            // 定义TrackerClient，用于请求TrackerServer
             TrackerClient trackerClient = new TrackerClient();
-            //连接tracker
+            // 连接tracker
             TrackerServer trackerServer = trackerClient.getConnection();
-            //获取Stroage
+            // 获取Stroage
             StorageServer storeStorage = trackerClient.getStoreStorage(trackerServer);
-            //创建stroageClient
-            StorageClient1 storageClient1 = new StorageClient1(trackerServer,storeStorage);
-            //向stroage服务器上传文件
-            //本地文件的路径
+            // 创建stroageClient
+            StorageClient1 storageClient1 = new StorageClient1(trackerServer, storeStorage);
+            // 向stroage服务器上传文件
+            // 本地文件的路径
             String filePath = "d:/logo.png";
-            //上传成功后拿到文件Id
+            // 上传成功后拿到文件Id
             String fileId = storageClient1.upload_file1(filePath, "png", null);
             System.out.println(fileId);
             //group1/M00/00/01/wKhlQVuhU3eADb4pAAAawU0ID2Q159.png
-
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 
-
-    //下载文件
+    // 下载文件
     @Test
-    public void testDownload(){
+    public void testDownload() {
         try {
-            //加载fastdfs-client.properties配置文件
+            // 加载fastdfs-client.properties配置文件
             ClientGlobal.initByProperties("config/fastdfs-client.properties");
-            //定义TrackerClient，用于请求TrackerServer
+            // 定义TrackerClient，用于请求TrackerServer
             TrackerClient trackerClient = new TrackerClient();
-            //连接tracker
+            // 连接tracker
             TrackerServer trackerServer = trackerClient.getConnection();
-            //获取Stroage
+            // 获取Stroage
             StorageServer storeStorage = trackerClient.getStoreStorage(trackerServer);
-            //创建stroageClient
-            StorageClient1 storageClient1 = new StorageClient1(trackerServer,storeStorage);
-            //下载文件
-            //文件id
+            // 创建stroageClient
+            StorageClient1 storageClient1 = new StorageClient1(trackerServer, storeStorage);
+            // 下载文件
+            // 文件id
             String fileId = "group1/M00/00/01/wKhlQVuhU3eADb4pAAAawU0ID2Q159.png";
             byte[] bytes = storageClient1.download_file1(fileId);
-            //使用输出流保存文件
+            // 使用输出流保存文件
             FileOutputStream fileOutputStream = new FileOutputStream(new File("c:/logo.png"));
             fileOutputStream.write(bytes);
         } catch (IOException e) {
@@ -77,6 +72,17 @@ public class TestFastDFS {
         }
     }
 
+    // 查询文件
+    @Test
+    public void testQueryFile() throws IOException, MyException {
+        ClientGlobal.initByProperties("config/fastdfs‐client.properties");
+        TrackerClient tracker = new TrackerClient();
+        TrackerServer trackerServer = tracker.getConnection();
+        StorageServer storageServer = null;
+        StorageClient storageClient = new StorageClient(trackerServer, storageServer);
+        FileInfo fileInfo = storageClient.query_file_info("group1", "M00/00/01/wKhlQFrKBSOAW5AWAALcAg10vf4862.png");
+        System.out.println(fileInfo);
+    }
 
 
 }
